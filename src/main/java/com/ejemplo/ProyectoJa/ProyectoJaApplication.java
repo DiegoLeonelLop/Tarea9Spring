0cd289a51dc5ab64f5a9d8006313d88dfa6c3b61
DiegoLeonelLop <diegosleoneslopez@gmail.com>
2023-05-05 23:23:50 -0600

    Archivos

---
+++ src/main/java/com/ejemplo/ProyectoJa/ProyectoJaApplication.java
@@ -0,0 +1,13 @@
+   package com.ejemplo.ProyectoJa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoJaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoJaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ejemplo/ProyectoJa/ProyectoJaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemplo.ProyectoJa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoJaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
